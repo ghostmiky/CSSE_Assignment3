636ee11d50f9c6da8a03863af2417caf08c28219
Malinda <shihandumidu1@gmail.com>
2018-10-01 15:42:08 +0530

    finxed git merge bug in MaterialREequestController.class

--- SpringBoot-sqlTest/src/main/java/com/sql/csse/ControllerManager/MaterialRequestController.java
+++ SpringBoot-sqlTest/src/main/java/com/sql/csse/ControllerManager/MaterialRequestController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.sql.csse.ControllerManager;
 
 
@@ -59,46 +59,3 @@ public class MaterialRequestController {
 
 
 }
-=======
-package com.sql.csse.ControllerManager;
-
-
-import com.sql.csse.EntityManager.MaterialRequest;
-import com.sql.csse.EntityManager.UsedMaterials;
-import com.sql.csse.RepositoryManager.MaterialRequestRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/Requests")
-public class MaterialRequestController {
-
-    @Autowired
-    MaterialRequestRepo materialRequestRepo;
-
-
-    @RequestMapping(method = RequestMethod.POST , value = "/save" , consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<MaterialRequest> MaterialRequests(@RequestBody final MaterialRequest mr){
-        materialRequestRepo.save(mr);
-        return materialRequestRepo.findAll();
-    }
-
-    @RequestMapping(method = RequestMethod.GET , value = "/getall" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MaterialRequest> getAll(){
-        return materialRequestRepo.findAll();
-    }
-
-
-    @RequestMapping(method = RequestMethod.GET , value = "/getPending" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MaterialRequest> findPending() {
-
-        return materialRequestRepo.findpendingRequests();
-    }
-}
->>>>>>> e85d61a71ff9d1a3e39933d515b6e76eeacfe8ae
