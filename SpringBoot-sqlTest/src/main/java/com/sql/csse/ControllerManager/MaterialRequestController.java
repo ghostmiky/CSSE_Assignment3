a75696166edf8baab6722f535de2733323a0e48c
sachuuuu <sachithasa@gmail.com>
2018-10-01 13:14:35 +0530

    Update the Pending request query,materialRepo and the controller

--- SpringBoot-sqlTest/src/main/java/com/sql/csse/ControllerManager/MaterialRequestController.java
+++ SpringBoot-sqlTest/src/main/java/com/sql/csse/ControllerManager/MaterialRequestController.java
@@ -1,15 +1,16 @@
 package com.sql.csse.ControllerManager;
 
 
+import com.google.gson.Gson;
 import com.sql.csse.EntityManager.MaterialRequest;
+import com.sql.csse.EntityManager.Order;
+import com.sql.csse.EntityManager.Supplier;
 import com.sql.csse.RepositoryManager.MaterialRequestRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,10 +20,15 @@ public class MaterialRequestController {
     @Autowired
     MaterialRequestRepo materialRequestRepo;
 
+    MaterialRequest materialRequest;
+    List<MaterialRequest> materialList;
 
     @RequestMapping(method = RequestMethod.POST , value = "/save" , consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<MaterialRequest> MaterialRequests(@RequestBody final MaterialRequest mr){
-        materialRequestRepo.save(mr);
+    public List<MaterialRequest> MaterialRequests(@RequestBody  String mr){
+
+        Gson gson = new Gson();
+        materialRequest =  gson.fromJson(mr, MaterialRequest.class);
+        materialRequestRepo.save(materialRequest);
         return materialRequestRepo.findAll();
     }
 
@@ -30,4 +36,25 @@ public class MaterialRequestController {
     public List<MaterialRequest> getAll(){
         return materialRequestRepo.findAll();
     }
+
+    @RequestMapping(method = RequestMethod.GET , value = "/getallPending" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<MaterialRequest> getPendingRequests(){
+
+        return materialRequestRepo.findpendingRequests();
+
+
+    }
+
+//    @RequestMapping(method = RequestMethod.GET , value = "/getPending/{status}" ,produces = MediaType.APPLICATION_JSON_VALUE)
+//    public MaterialRequest getPendingRequests(@PathVariable("status") String st){
+//        materialList = materialRequestRepo.findAll();
+//
+//        for(MaterialRequest mr : materialList){
+//            if(mr.getStatus() == st)
+//                materialRequest = mr;
+//        }
+//        return materialRequest;
+//    }
+
+
 }
--- SpringBoot-sqlTest/src/main/java/com/sql/csse/RepositoryManager/MaterialRequestRepo.java
+++ SpringBoot-sqlTest/src/main/java/com/sql/csse/RepositoryManager/MaterialRequestRepo.java
@@ -1,7 +1,6 @@
 package com.sql.csse.RepositoryManager;
 
 import com.sql.csse.EntityManager.MaterialRequest;
-import com.sql.csse.EntityManager.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,5 +10,5 @@ public interface MaterialRequestRepo extends JpaRepository<MaterialRequest, Inte
 
 
     @Query(value = "SELECT m FROM material__requests m WHERE lower(m.status) = lower('pending') ",nativeQuery = true)
-    public ArrayList<Order> findpendingRequests( );
+    public ArrayList<MaterialRequest> findpendingRequests();
 }
