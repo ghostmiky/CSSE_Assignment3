f6e68f764ef744568d9bef260883064d022d4674
Bhagya Sithumini <b.sithumini@yahoo.com>
2018-10-07 15:41:13 +0530

    findApprovedRequests

--- SpringBoot-sqlTest/src/main/java/com/sql/csse/RepositoryManager/MaterialRequestRepo.java
+++ SpringBoot-sqlTest/src/main/java/com/sql/csse/RepositoryManager/MaterialRequestRepo.java
@@ -34,5 +34,11 @@ public interface MaterialRequestRepo extends JpaRepository<MaterialRequest, Inte
     @Query(value = "UPDATE materila_requests m SET m.mid = :mid ,m.material_name=:material_name, m.material_quantity=:material_quantity, m.requested_date=:requested_date, m.order_date=:order_date, m.status=:status WHERE m.rid = :rid", nativeQuery = true)
     void updateRequest(@Param("rid") int rid, @Param("mid") int mid, @Param("material_name") String material_name, @Param("material_quantity") double material_quantity, @Param("requested_date") String requested_date, @Param("order_date") String order_date, @Param("status") String status);
 
+     /*
+     * This query is used for select the
+     * approved request details
+     * */
+    @Query(value = "SELECT * FROM material_requests m WHERE m.status = 'approved' ",nativeQuery = true)
+    ArrayList<MaterialRequest> findApprovedRequests( );
 
 }
